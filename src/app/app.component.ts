078f3fe519d8ff6ac06cbbcbcdfebd37284422a3
Perumal, Arulvelavan - MiDC (Cognizant) <Arulvelavan.Perumal@Chubb.com>
2019-07-22 20:58:57 +1000

    final changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,6 @@ export class AppComponent implements OnInit {
     ageIsValid: boolean;
     birthAge: number;
     api: GetFactor;
-    occtest: number;
     constructor(private formBuilder: FormBuilder) { }
 
     // Form fields
